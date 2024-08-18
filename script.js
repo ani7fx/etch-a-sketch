e68840bd6557f743738a4c257a90fa7b94a45b32
Anirudh Madhavan <madhavaa@tcd.ie>
2024-08-18 22:44:28 +0200

    created js and css sheets, and initial version of grid

---
+++ script.js
@@ -0,0 +1,8 @@
+const gridContiner = document.querySelector(".grid-container")
+
+for (i = 0; i < 256; i++){
+    const div = document.createElement("div")
+    div.classList.add("square")
+    div.innerText = "b"
+    gridContiner.appendChild(div)
+}
