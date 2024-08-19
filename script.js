b87f46aeb5f4088217d25f34eb7d0a5f06172fe3
Anirudh Madhavan <madhavaa@tcd.ie>
2024-08-19 22:52:35 +0200

    made a 16x16 grid

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const gridContiner = document.querySelector(".grid-container")
 
-for (i = 0; i < 256; i++){
-    const div = document.createElement("div")
-    div.classList.add("square")
-    div.innerText = "b"
-    gridContiner.appendChild(div)
+for (i = 0; i < 16; i++){
+    const rowDiv = document.createElement("div")
+    rowDiv.classList.add("row")
+    gridContiner.appendChild(rowDiv)
+    for (j = 0; j < 16; j++){
+        const Coldiv = document.createElement("div")
+        Coldiv.classList.add("square")
+        Coldiv.style.width = 960/16;
+        Coldiv.style.height = 960/16;
+        rowDiv.appendChild(Coldiv)
+    }
 }
